f00731e7fbbb9dcf3079abbf5b0e1961b6a958a1
PBB <pbbsignup@outlook.com>
2017-08-02 23:50:52 +0800

    1. 获得目录信息，删除旧目录（生成新目录还没做） 2. 记录了一些脑洞功能😂

--- Fletch.sketchplugin/Contents/Sketch/main.js
+++ Fletch.sketchplugin/Contents/Sketch/main.js
@@ -23,10 +23,15 @@ function prepareSelectedArtboards (context) {
 
 function updatePageNumbersOfArtboards (artboards) {
 
+    // 结果用于存储目录信息，以及是否更新成功
+    var result = {};
+    result.isSuccess = false;
+    result.data = {};
+
 	// 未选择图层提示
 	if (artboards.length == 0) {
-		NSApp.displayDialog("请选择文档的所有画板（包括封面和概述）");
-		return false;
+		[NSApp displayDialog: "请选择文档的所有画板（包括封面和概述）" withTitle: "页码更新失败"];
+		return result;
 	} 
 
 	// 获取要修改的图层 ID
@@ -38,8 +43,8 @@ function updatePageNumbersOfArtboards (artboards) {
 
         for (var i = 0; i < [children count]; i++){
             var layer = children[i];
-            if( layer.name() == "3" )   { layerIDs.currentPage_ID   = layer.objectID() }
-            if( layer.name() == "10" )    { layerIDs.totalPages_ID    = layer.objectID() }
+            if( layer.name() == "3" )   { layerIDs.currentPage_ID = layer.objectID() }
+            if( layer.name() == "10" )    { layerIDs.totalPages_ID = layer.objectID() }
         }
         return layerIDs;
     }
@@ -64,17 +69,21 @@ function updatePageNumbersOfArtboards (artboards) {
                     if (layerIDs == null ) {
                         layerIDs = getLayerIDs(pageNumberLayer);
                         if (layerIDs == null) {
-                            NSApp.displayDialog("页码图层需为 symbol");
-		                    return;
+                            [NSApp displayDialog: "页码图层需为 symbol" withTitle: "页码更新失败"];
+		                    return result;
                         }
                     }
                 }
             }
 
             if (pageTitleLayer == null || pageNumberLayer == null) {
-                	NSApp.displayDialog("未找到功能概述或页码");
-                	return;
-                }
+                [NSApp displayDialog: "功能概述的图层名需为“功能概述”，页码的图层名需为“交互图例 / 页码”" withTitle: "页码更新失败"];
+                return result;
+            }
+
+            // 将数据写入 result
+            result.data[i] = pageTitleLayer.stringValue();
+
 
             // 设定页码值
             var pageData = {};
@@ -89,7 +98,8 @@ function updatePageNumbersOfArtboards (artboards) {
             pageNumberLayer.frame().setY(pageTitleLayer.frame().y() - 2)
 		}
 	}
-	return true;
+    result.isSuccess = true;
+	return result;
 }
 
 var updatePageNumbers = function (context) {
@@ -101,9 +111,29 @@ var updatePageNumbers = function (context) {
 var updateCatalog = function (context) {
     var selectedArtboards = prepareSelectedArtboards(context);
     var result = updatePageNumbersOfArtboards(selectedArtboards);
-    if (!result) { return }
-        
+    if (!result.isSuccess) { return }
+       
+    // 处理重复标题，生成目录数据
+    var catalog = {};
+    var lastTitle = "";
+    for (var page in result.data) {
+        if (result.data[page].localeCompare(lastTitle) != 0) {
+            catalog[page] = result.data[page];
+        }
+        lastTitle = result.data[page];
+    }
+
     // 清理旧目录
+    var coverArtboard = selectedArtboards[0];
+    var layersInCoverArtboard = coverArtboard.children();
+    for (var i = 0; i < layersInCoverArtboard.length; i++) {
+        if (layersInCoverArtboard[i].name() == "目录一" || 
+            layersInCoverArtboard[i].name() == "目录二" ||
+            layersInCoverArtboard[i].name() == "目录三" ||
+            layersInCoverArtboard[i].name() == "目录四" ) {
+            layersInCoverArtboard[i].removeFromParent();
+        }
+    }
 
     // 生成新目录
 
